6064fc122c661346fdae2510b1926bbf5c7bc9da
Krasimir Velichkov <krasimirvelichkov@gmail.com>
2016-07-07 20:41:05 +0300

    Checkpoint commit

--- device-client/www/js/controllers/home.controller.js
+++ device-client/www/js/controllers/home.controller.js
@@ -1,14 +1,15 @@
 <!--home page for login/register-->
 angular.module('starter')
-  .controller('HomeCtrl', function ($scope, $state, $stateParams, $log, $ionicModal, $ionicSlideBoxDelegate, StatusFactory) {
+  .controller('HomeCtrl', function ($scope, $state, $stateParams, $log, $ionicModal, $ionicSlideBoxDelegate, StatusFactory, LoginService) {
+
+    $scope.isLogged = false;
+    
+    $scope.$watch('isLogged', function () {
+      $log.info('isLogged is now ' + $scope.isLogged);
+    });
 
     //Modal for login/register options
     //I think that it is better than tabs
-
-    $scope.slideHasChanged = function (index) {
-      $log.info(index);
-    };
-
     $scope.registerInfo = {
       name: '',
       email: '',
@@ -24,8 +25,45 @@ angular.module('starter')
     };
 
     $scope.loginInfo = {
-      email: '',
-      password: ''
+      email: 'krasimirvelichkov@gmail.com',
+      password: '123'
+    };
+
+    $scope.login = function () {
+      LoginService.loginUser($scope.loginInfo.email, $scope.loginInfo.password)
+        .success(function (result) {
+          // $log.info(result);
+          $scope.isLogged = true;
+        });
+    };
+
+
+    //Improve validation with watch
+    $scope.slideHasChanged = function (index) {
+      switch (index) {
+        //Case 1 : Name Email Password and Phone number
+        case 1:
+
+          break;
+
+        //Case 2 : Address, Date of Birth, Gender and Interests
+        case 2:
+          break;
+
+        //Case 3 : Add picture TODO implement file upload and link save in the DB
+        case 3:
+          break;
+
+        //Case 4 : Add social network
+        case 4:
+          break;
+
+        //Case 5: Check the details and register
+        case 5:
+          break;
+      }
+
+      $log.info(index);
     };
 
     //Init the register modal
@@ -44,9 +82,18 @@ angular.module('starter')
       $scope.loginModal = modal;
     });
 
+    $scope.disableSwipe = function () {
+      $ionicSlideBoxDelegate.enableSlide(false);
+    };
+
     $scope.nextSlide = function () {
       $ionicSlideBoxDelegate.next();
     };
+
+    $scope.previousSlide = function () {
+      $ionicSlideBoxDelegate.previous();
+    };
+
     $scope.callRegisterModal = function () {
       $scope.registerModal.show();
     };
