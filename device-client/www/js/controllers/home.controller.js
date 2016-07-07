5dc81ed94cbc9fc4695d63db130ad7b7a4ed0f38
Krasimir Velichkov <krasimirvelichkov@gmail.com>
2016-07-07 16:28:16 +0300

    Updating the register modal

--- device-client/www/js/controllers/home.controller.js
+++ device-client/www/js/controllers/home.controller.js
@@ -1,22 +1,60 @@
 <!--home page for login/register-->
 angular.module('starter')
-  .controller('HomeCtrl', function ($scope, $state, $stateParams, $log, $ionicModal, StatusFactory) {
+  .controller('HomeCtrl', function ($scope, $state, $stateParams, $log, $ionicModal, $ionicSlideBoxDelegate, StatusFactory) {
 
     //Modal for login/register options
     //I think that it is better than tabs
+
+    $scope.slideHasChanged = function (index) {
+      $log.info(index);
+    };
+
+    $scope.registerInfo = {
+      name: '',
+      email: '',
+      password: '',
+      phone: '',
+      address: '',
+      dateOfBirth: '',
+      gender: '',
+      interests: '',
+
+      //TODO implement picture logic
+      picture: ''
+    };
+
+    $scope.loginInfo = {
+      email: '',
+      password: ''
+    };
+
+    //Init the register modal
+    $ionicModal.fromTemplateUrl('templates/modals/register-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function (modal) {
+      $scope.registerModal = modal;
+    });
+
+    // Init the login modal
     $ionicModal.fromTemplateUrl('templates/modals/login-modal.html', {
       scope: $scope,
       animation: 'slide-in-up'
     }).then(function (modal) {
-      $scope.modal = modal;
-      // if (!StatusFactory.isLogged) {
-      //   modal.show();
-      // }
-
+      $scope.loginModal = modal;
     });
-    $scope.callModal = function () {
-      $scope.modal.show();
+
+    $scope.nextSlide = function () {
+      $ionicSlideBoxDelegate.next();
     };
+    $scope.callRegisterModal = function () {
+      $scope.registerModal.show();
+    };
+
+    $scope.callLoginModal = function () {
+      $scope.loginModal.show();
+    };
+
 
     $scope.goToLogin = function () {
       $state.go('tab.login');
