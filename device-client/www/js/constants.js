4af43cafcb9bee82af74cff04a91332455c4dbf2
Alexander Topalov <ex3m@mail.bg>
2016-08-10 20:59:04 +0300

    Stop tracking constant.js

--- device-client/www/js/constants.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by jumperabg on 15.6.2016 г..
- */
-
-angular.module('starter.constants', [])
-  .constant('SERVER_ADDRESS', 'http://dareornot.herokuapp.com')
-  .constant('SOCKET_CHAT_PORT', ':1337')
