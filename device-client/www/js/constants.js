bebdc755832fed8e9cad834437441b0c906d7f68
Krasimir Velichkov <krasimirvelichkov@gmail.com>
2016-08-24 18:38:47 +0300

    .gitignore is now working

--- device-client/www/js/constants.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by jumperabg on 15.6.2016 г..
- */
-
-angular.module('starter.constants', [])
-  .constant('SERVER_ADDRESS', 'http://dareornot.herokuapp.com')
-  .constant('SOCKET_CHAT_PORT', '')
-  .constant('SERVER_PORT', '')
